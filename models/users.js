9ef838b167b6044f134ef32e87eddaba04d43a7f
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-05-02 09:39:05 -0700

    initial commit

---
+++ models/users.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  id: String,
+  user: String,
+  deviceId: String,
+  sensors: [
+    {
+      sensorId: String, // Check against comPortId
+      comPortId: String, // Assigned to specific device id
+      schedule: [{ duration: Number, days: [{ day: String, time: Number }] }],
+      name: String
+    }
+  ]
+});
+
+module.exports = mongoose.model("User", userSchema);
