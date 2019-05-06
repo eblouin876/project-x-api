cface65f726fc85daf56b47a98822ad03de6c76e
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-05-06 12:58:14 -0700

    added placeholders for other api routes

--- controllers/pi-routes.js
+++ controllers/pi-routes.js
@@ -5,15 +5,16 @@ const broadcast = require("../config/middleware/broadcast");
 
 module.exports = function (app) {
 
-  app.get("/api/getUpdate", isAuthenticated, broadcast, function (req, res) { // NOTE: Does not require any req.body - reads all the paramas off the authenticated user
+  app.get("/api/arduinos", isAuthenticated, function (req, res) { // NOTE: Does not require any req.body - reads all the paramas off the authenticated user
     Users.findOne({id: req.user.id}, (err, data) => {
       if (err) console.log(err);
-      console.log(data);
-      res.send(data);
+      console.log(data.sensors);
+      res.send(data.sensors);
     });
   });
 
-  app.post("/api/addSensor", isAuthenticated, broadcast, function(req, res) {
+  // TODO: Add a new device
+  app.post("/api/addArduino", isAuthenticated, broadcast, function(req, res) {
     // NOTE: Needs to be passed sensorId in  req.body
     let SID = req.body.sensorId;
     let UID = req.user.id
@@ -42,45 +43,9 @@ module.exports = function (app) {
     });
   });
 
-
+  app.post("/api/updateArduino", isAuthenticated, broadcast, function(req,res) {});
   //   TODO: Add an update schedule route implementing findOneAndUpdate https://mongoosejs.com/docs/api.html#model_Model.findOneAndUpdate
 
+  app.delete("/api/deleteArduino", isAuthenticated, broadcast, function(req,res) {});
 
-
-  //  TODO Used for testing locally
-  // app.get("/api/getUpdate", function(req, res) {
-  //   Users.findOne({ id: "5cc925b47e5939ae39b43151" }, (err, data) => {
-  //     if (err) console.log(err);
-  //     console.log(data.sensors);
-  //     res.send(data.sensors);
-  //   });
-  // });
-
-  // app.post("/api/addSensor", function(req, res) {
-  //   // NOTE: Needs to be passed sensorId in  req.body
-  //   let SID = req.body.sensorId;
-  //   Users.findOne({ id: "5cc925b47e5939ae39b43151" }, (err, data) => {
-  //     if (err) console.log(err);
-  //     log(data);
-  //     let exists = false;
-  //     data.sensors.forEach(sensor => {
-  //       if (sensor.sensorId === SID) {
-  //         exists = true;
-  //         res.send("Sensor already exists");
-  //       }
-  //     });
-  //
-  //     if (!exists) {
-  //       Users.updateOne(
-  //           { id: "5cc925b47e5939ae39b43151" },
-  //           { $push: { sensors: { sensorId: SID } } },
-  //           (err, data) => {
-  //             if (err) console.log(err);
-  //             log(data);
-  //             res.send(data);
-  //           }
-  //       );
-  //     }
-  //   });
-  // });
 };
--- models/users.js
+++ models/users.js
@@ -2,17 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  id: String,
-  user: String,
-  deviceId: String,
-  sensors: [
-    {
-      sensorId: String, // Check against comPortId
-      comPortId: String, // Assigned to specific device id
-      schedule: [{ duration: Number, days: [{ day: String, time: Number }] }],
-      name: String
-    }
-  ]
+    id: String,
+    user: String,
+    deviceId: String,
+    discovery: {type: Boolean, default: false},
+    sensors: [
+        {
+            sensorId: String, // Check against comPortId
+            comPortId: String, // Assigned to specific device id
+            schedule: [{duration: Number, days: [{day: String, time: Number}]}],
+            name: String
+        }
+    ]
 });
 
 module.exports = mongoose.model("User", userSchema);
