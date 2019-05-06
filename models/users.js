04a2e9af5de6876bceebefd1155896279b3b0014
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-05-06 13:05:29 -0700

    Updated users model

--- models/users.js
+++ models/users.js
@@ -4,16 +4,19 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     id: String,
     user: String,
-    deviceId: String,
-    discovery: {type: Boolean, default: false},
-    sensors: [
-        {
-            sensorId: String, // Check against comPortId
-            comPortId: String, // Assigned to specific device id
+    piDevice: {
+        arduinos: [{
+            deviceId: String,
+            comName: String,
             schedule: [{duration: Number, days: [{day: String, time: Number}]}],
-            name: String
-        }
-    ]
+            plantName: String,
+            serialNumber: String,
+            status: Number
+        }],
+        status: Number,
+        serialNumber: String,
+        discovery: {type: Boolean, default: false},
+    }
 });
 
 module.exports = mongoose.model("User", userSchema);
