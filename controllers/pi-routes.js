40e3134d5ddb9ba9fc52a5a78e4ce45e02249871
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-05-09 19:02:00 -0700

    modified pi routes

--- controllers/pi-routes.js
+++ controllers/pi-routes.js
@@ -13,52 +13,24 @@ module.exports = function (app) {
         });
     });
 
-    // Route for the pi to hit in order to pull updates
-    app.get("/api/getUpdate/:id", isAuthenticated, function (req, res) {
-
-    });
-
-    // Route to add a new arduino to the account
-    app.post("/api/addArduino", isAuthenticated, broadcast, function (req, res) {
-        // NOTE: Needs to be passed sensorId in  req.body
-        let SID = req.body.sensorId;
-        let UID = req.user.id;
-        Users.findOne({id: UID}, (err, data) => {
-            if (err) console.log(err);
-            log(data);
-            let exists = false;
-            data.sensors.forEach(sensor => {
-                if (sensor.sensorId === SID) {
-                    exists = true;
-                    res.send("Sensor already exists");
-                }
-            });
-
-            if (!exists) {
-                Users.updateOne(
-                    {id: UID},
-                    {$push: {sensors: {sensorId: SID}}},
-                    (err, data) => {
-                        if (err) console.log(err);
-                        log(data);
-                        res.send(data);
-                    }
-                );
-            }
-        });
-    });
-
     // Route to update an existing arduino (for schedule)
     app.post("/api/updateArduinos", isAuthenticated, broadcast, function (req, res) {
         Users.findOneAndUpdate({id: req.user.id}, {piDevice: req.body})
             .catch(err => {
                 if (err) console.log(err)
-            }) .then(data => res.send(data));
+            }).then(data => res.send(data));
     });
-    //   TODO: Add an update schedule route implementing findOneAndUpdate https://mongoosejs.com/docs/api.html#model_Model.findOneAndUpdate
 
-    //Route to remove an arduino from the account
-    app.delete("/api/deleteArduino", isAuthenticated, broadcast, function (req, res) {
-    });
+    // Route to update single arduino. Needs schedule, plantName, and deviceId in the req.body
+    app.post("/api/updateArduino", isAuthenticated, broadcast, function (req, res) {
+        Users.findOneAndUpdate({id: req.user.id, "piDevice.deviceId": req.body.deviceId},
+            {"piDevice.$.schedule": req.body.schedule,
+            "piDevice.$.plantName": req.body.plantName
+            } )
+            .catch(err => {
+                if(err) console.log(err)
+            }).then (data => res.send(data))
+    })
+
 
 };
